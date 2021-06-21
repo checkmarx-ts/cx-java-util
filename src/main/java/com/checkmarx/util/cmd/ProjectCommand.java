checkmarx-ts/cx-java-util

commit 498269bc13e3c7d1da7b6f5e4fe332f06b1be8af

Improve error reporting
If a project is not found, throw an exception immediately instead of
passing an invalid project identifier (-1) to subsequent API calls.

src/main/java/com/checkmarx/util/cmd/ProjectCommand.java
@@ -34,7 +34,9 @@ public class ProjectCommand implements Callable<Integer> {
     private final CxService cxService;
     private final CxProperties cxProperties;
 
-    private final static String UNKNOWN_TEAM = "-1";
+    // In some cases the SDK returns an integer, in others a string ...
+    private final static int UNKNOWN_INT = -1;
+    private final static String UNKNOWN_STR = "-1";
 
     @Spec
     private CommandSpec spec;
@@ -241,10 +243,13 @@ public class ProjectCommand implements Callable<Integer> {
         if (team != null) {
             team = addTeamPathSeparatorPrefix(cxProperties, team);
             String teamId = getTeamId(team);
-            if (UNKNOWN_TEAM.equals(teamId)) {
+            if (UNKNOWN_STR.equals(teamId)) {
         	throw new CheckmarxException(String.format("getCxProjects: %s: no matching team", team));
             }
             Integer projectId = cxService.getProjectId(teamId, project);
+            if (UNKNOWN_INT == projectId) {
+                throw new CheckmarxException(String.format("getCxProjects: %s: no matching project", project));
+            }
             cxProject = cxService.getProject(projectId);
             cxProjects = new ArrayList<>();
             if (cxProject != null) {
@@ -274,7 +279,7 @@ public class ProjectCommand implements Callable<Integer> {
             log.error("getTeamId: error retrieving teams", e);
         }
         log.info("No team was found for {}", teamPath);
-        return UNKNOWN_TEAM;
+        return UNKNOWN_STR;
 
     }
 }

commit 5aa061ed3d108cd25ff741d947a130f5799289c6

Fix regression introduced by 498269bc1
If getProjectId() returns -1 (project not found), we do not want to
throw an exception, as this is a valid scenario. However, we do not
want to try to retrieve a project with id -1 as, by definition, does
not exist so we sip the call to getProject().

src/main/java/com/checkmarx/util/cmd/ProjectCommand.java
@@ -246,14 +246,13 @@ public class ProjectCommand implements Callable<Integer> {
             if (UNKNOWN_STR.equals(teamId)) {
         	throw new CheckmarxException(String.format("getCxProjects: %s: no matching team", team));
             }
-            Integer projectId = cxService.getProjectId(teamId, project);
-            if (UNKNOWN_INT == projectId) {
-                throw new CheckmarxException(String.format("getCxProjects: %s: no matching project", project));
-            }
-            cxProject = cxService.getProject(projectId);
             cxProjects = new ArrayList<>();
-            if (cxProject != null) {
-        	cxProjects.add(cxProject);
+            Integer projectId = cxService.getProjectId(teamId, project);
+            if (UNKNOWN_INT != projectId) {
+                cxProject = cxService.getProject(projectId);
+                if (cxProject != null) {
+            	   cxProjects.add(cxProject);
+                }
             }
         } else {
             cxProjects = cxService.getProjects();